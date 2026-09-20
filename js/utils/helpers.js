971d333ff0761fedd528aa55dd9a3fb9a4cb1c0b
profpurpleheartx <solimeno.irene@gmail.com>
2026-09-20 20:07:44 +0200

    Delete js directory

--- js/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-
-export function initials(name = "") {
-  return name.split(/\s+/).filter(Boolean).slice(0,2).map(x => x[0]).join("").toUpperCase();
-}
-export function isImagePath(path = "") {
-  return /\.(avif|gif|jpe?g|png|webp|svg)$/i.test(path);
-}
